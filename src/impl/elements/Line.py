2d5d05938ee21a32536a0bd2d30ebc12f8483718
milanpiskla <milan.piskla@gmail.com>
2024-09-24 17:00:19 +0200

    Forgot to stage Line.py

--- src/impl/elements/Line.py
+++ src/impl/elements/Line.py
@@ -1,7 +1,17 @@
 from ..base.AbstractElement import AbstractElement
 from ...config.Config import Config, DEFAULT_CONFIG
+from typing import Self
 
 class Line(AbstractElement):
     def __init__(self, config: Config = DEFAULT_CONFIG):
         super().__init__("line", config)
         self.points = config.get("points", [])
+
+    def plot(self, points: list[tuple[float, float]]) -> Self:
+        self.points = points
+        return self
+
+    def color(self, color: str) -> Self:
+        """Set the stroke (outline) color."""
+        self.strokeColor = color
+        return self
