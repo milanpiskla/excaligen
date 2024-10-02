13ee2d9c43ac73e8b24b9426d9664b975f5b2a10
Milan Piskla <milan.piskla@gmail.com>
2024-10-02 21:04:58 +0200

    Added more functional tests

--- tests/functional/test_functional.py
+++ tests/functional/test_functional.py
@@ -47,7 +47,31 @@ def test_texts(reference_json: Dict[str, Any]) -> None:
     xd = Excalidraw()
     xd.text().content("Hello, World!").fontsize("L").font("Hand-drawn").align("center").baseline("top").spacing(1.5).color("#FF0000")
     xd.text().position(100, 100).content("Hello, Excalifont!").fontsize(40).font("excalifont").align("center").baseline("top").spacing(1.5).color("#0000FF").autoresize(True)
-    xd.save('texts.excalidraw')
 
     evaluate(reference_json, xd)
 
+def test_labels(reference_json: Dict[str, Any]) -> None:
+    xd = Excalidraw()
+    label = xd.text().content("Hello, World!").fontsize("M").font("Hand-drawn").spacing(1.5).color("#FF0000")
+    xd.rectangle().position(10, 20).size(300, 100).label(label)
+
+    evaluate(reference_json, xd)
+
+
+def test_sandbox(reference_json: Dict[str, Any]) -> None:
+    xd = Excalidraw()
+
+    def cross(center: tuple[float, float], color: str) -> None:
+        x, y = center
+        xd.line().plot([[x - 20, y], [x + 20, y]]).color(color)
+        xd.line().plot([[x, y - 20], [x, y + 20]]).color(color)
+
+    cross([0, 0], '#ff0000')
+    cross([100, 50], '#00ff00')
+
+    xd.ellipse().position(0, 0).size(50, 50)
+    xd.ellipse().position(0, 0).size(100, 100).fade(50)
+
+    xd.text().position(0, 0).content('Center').fontsize('L').align('center').baseline('middle').rotate(3.14 / 4)
+
+    evaluate(reference_json, xd)
