9443d478116076cd580eacdb38eb009b192190ac
Milan Piskla <milan.piskla@gmail.com>
2024-10-27 20:20:29 +0100

    Added test for arc arrow

--- tests/functional/test_functional.py
+++ tests/functional/test_functional.py
@@ -91,3 +91,25 @@ def test_lines(reference_json: Dict[str, Any], request: FixtureRequest) -> None:
     xd.line().color('#FF0000').points([(0, 0), (-100, -50), (-100, 50)]).sloppiness(0).stroke('dashed').roundness('round')
 
     evaluate(reference_json, xd, request)
+
+def test_arrow_arc(reference_json: Dict[str, Any], request: FixtureRequest) -> None:
+    xd = Excalidraw()
+
+    RADIUS = 300
+    elements = []
+
+    for angle in range(0, 360, 30):
+        radians = angle * math.pi / 180
+        x = RADIUS * math.cos(radians)
+        y = RADIUS * math.sin(radians)
+
+        #rect = xd.rectangle().center(x, y).size(80, 60)
+        rect = xd.ellipse().center(x, y).size(80, 60)
+        elements.append(rect)
+
+    start_element = elements[0]
+    for i in range(1, len(elements)):
+        xd.arrow().arc(radius=RADIUS).bind(start_element, elements[i])
+        start_element = elements[i]
+
+    evaluate(reference_json, xd, request)
