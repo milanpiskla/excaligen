7e7adb5265e07ef6dc615b71cb68cc1771e46aea
Milan Piskla <milan.piskla@google.com>
2026-02-10 23:12:06 +0100

    Improved teh surprise.py example. I'm so proud of that :)

--- examples/surprise.py
+++ examples/surprise.py
@@ -1,26 +1,20 @@
 """
 Description: Who will guess what this code will generate?
-This is not the style anyone should use for drawing diagrams, but the output will surprise you :)
+I'm so proud that I wrote this code :)
 """
 # Copyright (c) 2024 - 2026 Milan Piskla
 # Licensed under the MIT License - see LICENSE file for details
 
 from excaligen.SceneBuilder import SceneBuilder
-from math import floor
 
 s = SceneBuilder()
 
-M = 20
-A = 2*42**4 + 12*42**3 + 15*42**2 + 16*42 + 12
-B = 23*42**3 + 20*42**2 + 11*42 + 9
+D = 42 * (42 * (42 * (42 * (42 * (42 * (42 * (42 * 3) + 25) + 26) + 2) + 28) + 30) + 8)
 
-for y in range(8):
-    d = A if y < 4 else B
-    for x in range(11):
-        bs = floor(6 * (y % 4))
-        bo = bs + abs(5 - x)
-        c = floor(floor(d / pow(2.0, bo)) % 2.0)
-        if c == 1:
-            s.rectangle().position(x * M, y * M).size(M, M).color('red').background("red").fill("solid").roundness('sharp')
+for y42 in range(42 - 34):
+    for x42 in range(42 - 31):
+        o = (y42 * 42 // 7) + abs(42 - 37 - x42)
+        if (D >> o) & (42 - 41):
+            s.rectangle().position(x42 * 42, y42 * 42).size(42, 42).color('#ff4242').background("#ff4242").fill("solid").roundness('sharp').sloppiness('architect')
 
 s.save("surprise.excalidraw")
