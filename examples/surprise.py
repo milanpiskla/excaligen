49485952fa5e027aaef2714ab91a61d8cd9b1c5c
Milan Piskla <milan.piskla@gmail.com>
2026-02-11 17:01:12 +0100

    Still working on examples

--- examples/surprise.py
+++ examples/surprise.py
@@ -10,12 +10,10 @@ from excaligen.SceneBuilder import SceneBuilder
 s = SceneBuilder()
 
 D = 42 * (42 * (42 * (42 * (42 * (42 * (42 * (42 * 3) + 25) + 26) + 2) + 28) + 30) + 8)
-r42 = 42 - 0o42
 
-for y42 in range(r42):
-    for x42 in range(r42 + 3):
-        o = (y42 * 42 // 7) + abs(r42 - x42 - 42 // 14)
-        if (D >> o) & (42 - 41):
-            s.rectangle().position(x42 * 42, y42 * 42).size(42, 42).color('#ff4242').background("#ff4242").fill("solid").roundness('sharp').sloppiness('architect')
+for y in range(8):
+    for x in range(11):
+        if (D >> ((y * 6) + abs(5 - x))) & 1:
+            s.rectangle().position(x * 42, y * 42).size(42, 42).color('#ff4242').background("#ff4242").fill("solid").roundness('sharp').sloppiness('architect')
 
 s.save("surprise.excalidraw")
